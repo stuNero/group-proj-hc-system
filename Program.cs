2ccf4cce4e24ba58d5f074e64bcb70e0e262c81c
stuNero <max.vemic@student.nbi-handelsakademin.se>
2025-10-13 14:59:16 +0200

    add/ Main Menu option & Logout

--- Program.cs
+++ Program.cs
@@ -1,77 +1,98 @@
 ﻿using System.Diagnostics;
+using System.Reflection;
 using App;
 
-// Logout
 // Main Menu
+// Logout
 
 List<IUser> users = new();
-
 IUser? activeUser = null;
+Menu currentMenu = Menu.Default;
 
 bool isRunning = true;
 while (isRunning)
 {
-  try { Console.Clear(); } catch { }
-  Console.WriteLine("\n[1] Login \n[2] Register Account\n[3] Quit\n");
-  Console.Write("> ");
-  string? input = Console.ReadLine();
-
-  switch (input)
+  switch (currentMenu)
   {
-    case "1":
-      Console.Write("Please input your SSN: ");
-      string? ssn = Console.ReadLine();
-      Console.Write("Please input a password: ");
-      string? password = Console.ReadLine();
+    case Menu.Default:
+      try { Console.Clear(); } catch { }
+      Console.WriteLine("\n[1] Login \n[2] Register Account\n[3] Quit\n");
+      Console.Write("> ");
+      string? input = Console.ReadLine();
 
-      Debug.Assert(ssn != null);
-      Debug.Assert(password != null);
-
-      foreach (IUser user in users)
+      switch (input)
       {
-        if (user.TryLogin(ssn, password))
-        {
-          activeUser = user;
+        case "1":
+          Console.Write("Please input your SSN: ");
+          string? ssn = Console.ReadLine();
+          Console.Write("Please input a password: ");
+          string? password = Console.ReadLine();
+
+          Debug.Assert(ssn != null);
+          Debug.Assert(password != null);
+
+          foreach (IUser user in users)
+          {
+            if (user.TryLogin(ssn, password))
+            {
+              activeUser = user;
+              currentMenu = Menu.Main;
+              break;
+            }
+          }
           break;
-        }
-      }
-      if (activeUser is User u)
-      {
-        Console.WriteLine("Welcome " + u.Name);
-        Console.ReadLine();
-      }
-      break;
-    case "2":
-      Console.Write("Please input your SSN: ");
-      string? newSSN = Console.ReadLine();
-      if (string.IsNullOrWhiteSpace(newSSN))
-      {
-        Console.WriteLine("Invalid input");
-        Console.ReadLine();
-        break;
-      }
 
-      Console.Write("Please input a password: ");
-      string? newPassword = Console.ReadLine();
-      Console.Write("What is your name? ");
-      string? newName = Console.ReadLine();
-      if (string.IsNullOrWhiteSpace(newName))
-      {
-        Console.WriteLine("Invalid input");
-        Console.ReadLine();
-        break;
+        case "2":
+          Console.Write("Please input your SSN: ");
+          string? newSSN = Console.ReadLine();
+          if (string.IsNullOrWhiteSpace(newSSN))
+          {
+            Console.WriteLine("Invalid input");
+            Console.ReadLine();
+            break;
+          }
+
+          Console.Write("Please input a password: ");
+          string? newPassword = Console.ReadLine();
+          Console.Write("What is your name? ");
+          string? newName = Console.ReadLine();
+          if (string.IsNullOrWhiteSpace(newName))
+          {
+            Console.WriteLine("Invalid input");
+            Console.ReadLine();
+            break;
+          }
+          Debug.Assert(newSSN != null);
+          Debug.Assert(newPassword != null);
+          Debug.Assert(newName != null);
+          users.Add(new User(newSSN, newPassword, newName));
+          break;
+        case "3":
+          isRunning = false;
+          break;
+        default:
+          Console.WriteLine("Please enter a valid input");
+          Console.ReadLine();
+          break;
       }
-      Debug.Assert(newSSN != null);
-      Debug.Assert(newPassword != null);
-      Debug.Assert(newName != null);
-      users.Add(new User(newSSN, newPassword, newName));
       break;
-    case "3":
-      isRunning = false;
-      break;
-    default:
-      Console.WriteLine("Please enter a valid input");
-      Console.ReadLine();
+    case Menu.Main:
+      try{ Console.Clear(); } catch{}
+      Console.WriteLine("[1] Logout");
+
+      switch (Console.ReadLine())
+      {
+        case "1":
+          activeUser = null;
+          currentMenu = Menu.Default;
+          break;
+        default:
+          Console.WriteLine("Something went wrong");
+          Console.ReadLine();
+          break;
+      }
+            
       break;
   }
+
 }
