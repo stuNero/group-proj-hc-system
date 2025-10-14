010009b68a6c99353dc8b07444b7bf1eda42571c
Meti00a <muhamedhajra00@gmail.com>
2025-10-14 11:33:30 +0200

    Add/Participant class and Enum roles

---
+++ Participant.cs
@@ -0,0 +1,24 @@
+
+
+namespace App;
+
+class Participant
+{
+
+        public User User;
+    public Roles Userroles;
+    
+
+    
+
+}
+enum Roles
+{
+    Admin,
+    Patient,
+    Personnel,
+}
+    
+    
+     
+    
