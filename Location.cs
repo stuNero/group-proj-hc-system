591ab06b674e805db84190be9864821181a8131e
Piercor <pierinocr@gmail.com>
2025-10-14 11:15:54 +0200

    add/ Location class

---
+++ Location.cs
@@ -0,0 +1,13 @@
+namespace App;
+
+class Location
+{
+  public string Name;
+  public string Address;
+
+  public Location(string name, string address)
+  {
+    Name = name;
+    Address = address;
+  }
+}
