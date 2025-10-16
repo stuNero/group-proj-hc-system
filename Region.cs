c324aafaa8b55c5c574ae1caadc28a8d4348ca48
Piercor <pierinocr@gmail.com>
2025-10-16 15:16:15 +0200

    add/ Region enums

---
+++ Region.cs
@@ -0,0 +1,26 @@
+namespace App;
+
+enum Region
+{
+  Blekinge,
+  Dalarna,
+  Gotland,
+  Gävleborg,
+  Halland,
+  Jämtland,
+  Jönköping,
+  Kalmar,
+  Kronoberg,
+  Norrbotten,
+  Skåne,
+  Stockholm,
+  Sörmland,
+  Uppsala,
+  Värmland,
+  Västerbotten,
+  Västernorrland,
+  Västmandland,
+  VästraGötaland,
+  Örebro,
+  Östergötland,
+}
