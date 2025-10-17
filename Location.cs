f3e8badd2946d787e17a7628e2fce0d499b9c7d3
Piercor <pierinocr@gmail.com>
2025-10-17 11:49:26 +0200

    add/ Region enum to Location class

--- Location.cs
+++ Location.cs
@@ -4,9 +4,11 @@ class Location
 {
   public string Name;
   public string Address;
-  public Location(string name, string address)
+  public Region Region;
+  public Location(string name, string address, Region region = Region.None)
   {
     Name = name;
     Address = address;
+    Region = region;
   }
 }
